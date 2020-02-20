b033465c7e1adabdacde492e577dd1c18902b96b
natebwangsut <bhurinat.wangsutthitham@agoda.com>
2020-02-21 04:25:18 +0700

    Fix scores

--- online-qualification/go/common/library.go
+++ online-qualification/go/common/library.go
@@ -30,14 +30,11 @@ func (l Library) CalcScore(remainingDays uint32, signUpCost float32) float32 {
 		for _, book := range l.Books {
 		}*/
 
-		completeionCost := (float32(l.SignUp) + float32(canTake/l.Ship))
-
 		availBooks := l.Books[:canTake]
 		for _, book := range availBooks {
 			scores += float32(book.Score)
-			// print(scores)
 		}
-		return scores / completeionCost
+		return scores / float32(l.SignUp)
 	}
 	return -1
 }
